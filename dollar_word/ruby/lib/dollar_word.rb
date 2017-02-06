4871c3201384437fbfa9bb76fb2afe22e8ff064b
Adam Bourg <Adam.bourg@gmail.com>
2017-02-05 22:50:15 -0700

    got dollar to work with tdd specs

--- dollar_word/ruby/lib/dollar_word.rb
+++ dollar_word/ruby/lib/dollar_word.rb
@@ -1,4 +1,31 @@
 class DollarWord
 
+  def self.compute(options)
+    return {} if options.size <= 0
+    words = options[:words].downcase.split(' ')
+
+    total_words = words.size
+    dollar_words = 0
+
+    letter_positions = ("a".."z").to_a
+
+    words.each do |word|
+      word_price = 0
+      word.split("").each do |char|
+        word_price = letter_positions.index(char).to_i + 1 + word_price
+      end
+
+      if word_price == 100
+        dollar_words += 1
+      end
+
+      word_count = 0
+    end
+
+    {
+      word_count: total_words,
+      dollar_words: dollar_words
+    }
+  end
 
 end
--- dollar_word/ruby/spec/dollar_word_spec.rb
+++ dollar_word/ruby/spec/dollar_word_spec.rb
@@ -1,9 +1,58 @@
 require 'spec_helper'
 require 'dollar_word'
 
-
 describe DollarWord do
 
+  it 'returns default of 0 for no options given' do
+    expect(DollarWord.compute({}).size).to eq(0)
+  end
 
+  context 'returns a correct word count' do
+    it '0 words' do
+      compute = DollarWord.compute({words: ''})
+      expect(compute[:word_count]).to eq(0)
+    end
+
+    it '1 word' do
+      compute = DollarWord.compute({words: 'adam'})
+      expect(compute[:word_count]).to eq(1)
+    end
+
+    it '2 words' do
+      compute = DollarWord.compute({words: 'adam bourg'})
+      expect(compute[:word_count]).to eq(2)
+    end
+
+    it '3 words' do
+      compute = DollarWord.compute({words: 'adam robert bourg'})
+      expect(compute[:word_count]).to eq(3)
+    end
+  end
+
+  context 'real dollar words' do
+    it 'lower case example' do
+      compute = DollarWord.compute({words: 'james bond was grumpy'})
+      expect(compute[:word_count]).to eq(4)
+      expect(compute[:dollar_words]).to eq(1)
+    end
+
+    it 'upper case example' do
+      compute = DollarWord.compute({words: 'JAMES BOND WAS GRUMPY'})
+      expect(compute[:word_count]).to eq(4)
+      expect(compute[:dollar_words]).to eq(1)
+    end
+
+    it 'mixed case example' do
+      compute = DollarWord.compute({words: 'James Bond Was GrUmPy'})
+      expect(compute[:word_count]).to eq(4)
+      expect(compute[:dollar_words]).to eq(1)
+    end
+
+    it 'many dollar words' do
+      compute = DollarWord.compute({words: 'motors drossy toWers governS GrUmPy'})
+      expect(compute[:word_count]).to eq(5)
+      expect(compute[:dollar_words]).to eq(5)
+    end
+  end
 
 end
